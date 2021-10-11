c39807f3dc6e3cf6677278b8be65f6ea0a97a77a
JohnHolbanel <jholbanel97@gmail.com>
2021-10-11 22:04:00 +0100

    first part of demo. Setting up project

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into student
+values(10001,'David', 'E1234567');
+
+insert into student
+values(10002,'Jennifer', 'A1234568');
---
+++ src/test/java/com/example/jholbane/springboot2jdbcwithh2/SpringBoot2JdbcWithH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jholbane.springboot2jdbcwithh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2JdbcWithH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
